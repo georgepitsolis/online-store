c7c7e26d724e5c5e1a3799aecec87127255b3154
georgepitolis <el16441@central.ntua.gr>
2023-10-04 18:06:47 +0300

    product-card v2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,11 @@ import { ProductCardComponent } from './product-card/product-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule, MatButtonModule, MatIconModule, MatBadgeModule,
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent {
+ product = {name: 'Neckless', code: '123456', price: 12.34, description: 'Text for the product. Dammy text is here.'};
+ cartItem = {inCart: false, quantity: 0}
+ favorite = false;
+
+
+ onFavorite(): void {
+  this.favorite = !this.favorite;
+ }
+
+ addToCart() {
+  this.cartItem.inCart = true;
+  this.cartItem.quantity ++;
+  console.log(this.cartItem)
+ }
 
 }
